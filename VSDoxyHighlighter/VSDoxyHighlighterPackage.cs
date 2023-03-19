a67ecf1642fafbdc19426a3708b9a5d9b39461ed
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2023-03-19 17:52:18 +0100

    Incremented version to 1.2.0.

--- VSDoxyHighlighter/VSDoxyHighlighterPackage.cs
+++ VSDoxyHighlighter/VSDoxyHighlighterPackage.cs
@@ -15,7 +15,7 @@ namespace VSDoxyHighlighter
   [InstalledProductRegistration(
     "VSDoxyHighlighter",
     "Provides syntax highlighting and IntelliSense for doxygen/javadoc style comments in C/C++. Github page: https://github.com/Sedeniono/VSDoxyHighlighter", 
-    "1.1.1")]
+    "1.2.0")]
   
   // ProvideOptionPage: Causes VS to show a page in the VS options.
   // The resource IDs refer to the Resources.resx file.
