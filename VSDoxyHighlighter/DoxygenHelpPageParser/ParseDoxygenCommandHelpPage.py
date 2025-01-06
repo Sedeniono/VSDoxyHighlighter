abbb7612c831cecc73354358966f65ab2fd1ee34
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2025-01-06 17:10:17 +0100

    Generated command help texts from Doxygen v1.13.1.

--- VSDoxyHighlighter/DoxygenHelpPageParser/ParseDoxygenCommandHelpPage.py
+++ VSDoxyHighlighter/DoxygenHelpPageParser/ParseDoxygenCommandHelpPage.py
@@ -595,6 +595,6 @@ if __name__ == "__main__":
     extract_and_convert_doxygen_commands_from_html(
         # The input html file is https://www.doxygen.nl/manual/commands.html
         # (simply saved via a browser).
-        html_filename=os.path.join(main_folder, "testInput1.12.0.htm"),
+        html_filename=os.path.join(main_folder, "testInput1.13.1.htm"),
         output_csharp_filename=os.path.join(main_folder, "DoxygenCommandsGeneratedFromHelpPage.cs"),
         output_debug_dump_filename=os.path.join(main_folder, "GeneratedDebugDump.txt"))
